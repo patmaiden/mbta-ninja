7ee513b5d51440d3e6af61fbc6b8ffbd90897f1e
Nabil Hashmi <nabil2011@gmail.com>
2015-06-06 18:55:10 -0400

    Add alert icon on landing page for paths and lines with any alerts

---
+++ client/templates/line.js
@@ -0,0 +1,25 @@
+Meteor.subscribe('reports');
+
+Template.line.helpers({
+  numberReports: function() {
+    return Reports.find({
+      expired: false,
+      line: this.path
+    }).count();
+  },
+  lineAlerts: function(){
+    var directions = this.directions;
+    var anyAlerts = false;
+    for (var i = 0; i < directions.length; i++) {
+      var path = directions[i].path
+      var path_reports = Reports.find({
+        expired: false,
+        line: path
+      }).count();
+      if (path_reports) {
+        anyAlerts = true;
+      }
+    };
+    return anyAlerts;
+  }
+});
